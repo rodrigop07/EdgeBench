5dd5497f800b26b40f279d0a09c6db8eca43715f
rodrigop07 <rodrigopinheiro754@outlook.com>
2026-09-15 17:52:11 -0300

    fix: corrige aquisição do epoch

--- firmware/main/app_main.c
+++ firmware/main/app_main.c
@@ -27,7 +27,7 @@
 static const char *TAG = "APP_MAIN";
 
 void app_main(void) {
-    ESP_LOGI(TAG, "testeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Memoria livre: %" PRIu32 " bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF Versao: %s", esp_get_idf_version());
 
@@ -39,6 +39,10 @@ void app_main(void) {
     esp_log_level_set("LORA_RCV", ESP_LOG_INFO);
     esp_log_level_set("NVS_MGR", ESP_LOG_INFO);
     esp_log_level_set("OTA_MGR", ESP_LOG_INFO);
+
+    // configura fuso horário para GMT-3
+    setenv("TZ", "<-03>3", 1);
+    tzset();
 
     // inicializa o sistema NVS
     ESP_ERROR_CHECK(nvs_manager_init());
--- firmware/main/lora_receiver.c
+++ firmware/main/lora_receiver.c
@@ -4,6 +4,7 @@
 #include "esp_log.h"
 #include "esp_mac.h"
 #include "esp_rom_sys.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
 #include "freertos/task.h"
